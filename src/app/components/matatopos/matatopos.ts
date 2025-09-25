1a88253d81fbdfb39ec5e6eb7425b9a6f1e2ab38
Adrián González Blanco <z3u2thnder@proton.me>
2025-09-25 18:21:53 +0200

    matatopos terminado

--- src/app/components/matatopos/matatopos.ts
+++ src/app/components/matatopos/matatopos.ts
@@ -1,12 +1,26 @@
-import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, QueryList, ViewChildren } from '@angular/core';
+
 
 @Component({
   selector: 'app-matatopos',
-  imports: [NgFor],
+  imports: [NgFor, NgIf],
   templateUrl: './matatopos.html',
   styleUrl: './matatopos.css'
 })
 export class Matatopos {
   topos:number[][] = [[1,2,3],[4,5,6],[7,8,9]];
+  
+  activo:number = 4;
+
+  randomizar(btn:number){
+    if(btn === this.activo){
+      this.activo = Math.floor(Math.random() * 9);
+      console.log(this.activo);
+    }
+  }
+
+  ngOnInit(){
+    this.randomizar(this.activo);
+  }
 }
