eec1534b6d440572203e95277fe8b4d5dad93056
Adrián González Blanco <z3u2thnder@proton.me>
2025-10-15 01:40:57 +0200

    completeFinalizeCountDown

--- src/app/components/contador-time-hallowen/contador-time-hallowen.ts
+++ src/app/components/contador-time-hallowen/contador-time-hallowen.ts
@@ -17,9 +17,11 @@ export class ContadorTimeHallowen {
   private subscription?: Subscription;
 
   rTime:String [] = ['0',":",'0',":",'0',":","0"];
+  current:boolean = true;
 
   ngOnInit() {
     let endTime: number;
+    
 
     if (this.targetDate) {
       endTime = this.targetDate.getTime();
@@ -33,17 +35,26 @@ export class ContadorTimeHallowen {
         takeWhile(ms => ms > 0, true)
       )
       .subscribe(ms => {
-        this.rTime = this.formatTime(ms);
+        try {
+          this.rTime = this.formatTime(ms);
+        } catch (error) {
+          this.current = false;
+        }
+        
       });
   }
 
-  private formatTime(ms: number): String [] {
-    const totalSeconds = Math.floor(ms / 1000);
-    const days = Math.floor(totalSeconds / 86400)
-    const hours = Math.floor((totalSeconds % 86400) / 3600);
-    const minutes = Math.floor(((totalSeconds % 86400) % 3600) / 60);
-    const seconds = totalSeconds % 60;
-    return [days + 'd ',' - ',hours + ' h ',':',minutes + ' min ',':',seconds + ' seg'];
+  private formatTime(ms: number): String []{
+    if(ms > 0){
+      const totalSeconds = Math.floor(ms / 1000);
+      const days = Math.floor(totalSeconds / 86400)
+      const hours = Math.floor((totalSeconds % 86400) / 3600);
+      const minutes = Math.floor(((totalSeconds % 86400) % 3600) / 60);
+      const seconds = totalSeconds % 60;
+      return [days + 'd ',' - ',hours + ' h ',':',minutes + ' min ',':',seconds + ' seg'];
+    }else{
+      throw new Error("se Termino");
+    }
   }
 
   private pad(num: number): string {
